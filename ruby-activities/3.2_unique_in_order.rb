d5273f25a37de655584f207be997187dbcdc8e9f
lea927 <lxthym@icloud.com>
2021-07-08 23:23:02 +0800

    Updates the code block to fix issue with array with string items

--- ruby-activities/3.2_unique_in_order.rb
+++ ruby-activities/3.2_unique_in_order.rb
@@ -1,8 +1,9 @@
 # Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements
 # with the same value next to each other and preserving the original order of elements.
-
 def unique_in_order(chars) 
-  chars.is_a?(String) ? chars.squeeze.split(//) : chars.uniq
+  
+  chars.is_a?(String) ? chars.squeeze.split(//) : chars.keep_if.with_index { |item, index| item != chars[index+1] }
+
 end
 
 p unique_in_order('AAAABBBCCDAABB') == ['A','B','C','D','A','B']
